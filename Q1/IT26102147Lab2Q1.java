fb407f664e9c0363a6f20df0071a0595119ac16d
IT26102147 <it26102147@my.sliit.lk>
2026-06-15 09:17:04 +0530

    Add files via upload

---
+++ Q1/IT26102147Lab2Q1.java
@@ -0,0 +1,15 @@
+public class IT26102147Lab2Q1{
+  public static void main(String[] args){
+      int perimeter=100;
+	  double length;
+	  double width;
+	  
+	  length= (2*perimeter)/7.0;
+	  width= (3*perimeter)/4.0;
+	  
+	  System.out.println(length);
+	  System.out.println(width);
+	  
+  
+  }
+ }
