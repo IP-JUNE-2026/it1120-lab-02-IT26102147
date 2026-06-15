70c1bba2f385b2206f4446cef140e09211003e7a
IT26102147 <it26102147@my.sliit.lk>
2026-06-15 09:17:37 +0530

    Add files via upload

---
+++ Q2/IT26102147Lab2Q2.java
@@ -0,0 +1,10 @@
+public class IT26102147Lab2Q2{
+  public static void main(String[] args){
+      int length=10;
+	  double radius;
+	  
+	  radius=(7*length)/11.0;
+	  
+	  System.out.println("radius of the circular fence: "+radius);
+	  }
+}
